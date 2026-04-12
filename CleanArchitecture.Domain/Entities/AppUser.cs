20d315cac4b928f4708cd280430621b10f43e225
Erdinc Cetinkaya <chatinrock@gmaiil.com>
2026-04-12 15:36:56 +0300

    initial

---
+++ CleanArchitecture.Domain/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class AppUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => string.Join(" ", FirstName, LastName);
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpires { get; set; }
+    }
+}
